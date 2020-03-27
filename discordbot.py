865b4c18e3908405cd20fee71d0aa7055ceeccbd
zerokun1091 <62707674+zerokun1091@users.noreply.github.com>
2020-03-27 15:04:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,22 +17,5 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-@bot.event
-async def on_message(message):
-    # メンバーのリストを取得して表示
-    if message.content == '/members':
-        print(message.guild.members)
-    # 役職のリストを取得して表示
-    if message.content == '/roles':
-        print(message.guild.roles)
-    # テキストチャンネルのリストを取得して表示
-    if message.content == '/text_channels':
-        print(message.guild.text_channels)
-    # ボイスチャンネルのリストを取得して表示
-    if message.content == '/voice_channels':
-        print(message.guild.voice_channels)
-    # カテゴリチャンネルのリストを取得して表示
-    if message.content == '/category_channels':
-        print(message.guild.categories)
 
 bot.run(token)
