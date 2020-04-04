a281b570dd4016d00283105ed4a6a6b0087275f6
zerokun1091 <62707674+zerokun1091@users.noreply.github.com>
2020-04-05 01:18:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,15 +9,13 @@ client = discord.Client()
 async def on_ready():
     print('ログインしました')
 
-# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
+ 
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
-# Botの起動とDiscordサーバーへの接続
+
 client.run(TOKEN)
