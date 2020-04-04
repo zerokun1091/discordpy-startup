622e91f20cea6b98ce881d0f1b0ca06449236983
zerokun1091 <62707674+zerokun1091@users.noreply.github.com>
2020-04-04 14:05:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,8 +12,6 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-   await bot.change_presence(game=discord.Game(name="Test", type=1))
-    print ("Started")
 
 
 @bot.command()
