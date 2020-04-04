f60dab08f01b5ec147fd973a5f90cff17d34ff4f
zerokun1091 <62707674+zerokun1091@users.noreply.github.com>
2020-04-04 21:12:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,25 +2,25 @@ from discord.ext import commands
 import os
 import traceback
 
-client = commands.Bot(command_prefix='.')
+bot = commands.Bot(command_prefix='.')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@client.event
+@bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@client.event
+@bot.event
 async def on_ready():
     print ("Starting up")
 activity = discord.Activity(name='python', type=discord.ActivityType.watching)
 await client.change_presence(activity=activity)
 
-@client.command()
+@bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 
-client.run(token)
+bot.run(token)
