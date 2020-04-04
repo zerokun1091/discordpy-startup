6d965542dbf627ac3d5a1e9a6c0001d7f3e9012d
zerokun1091 <62707674+zerokun1091@users.noreply.github.com>
2020-04-04 14:20:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,6 +12,11 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+async def on_ready():
+    print ("Starting up")
+    print ("My username is " + bot.user.name + " and i am running with the ID: " + bot.user.id)
+    await bot.change_presence(game=discord.Game(name="Test", type=1))
+    print ("Started")
 
 
 @bot.command()
