a3a49c87ee409c1be4b5c9b30b911b2915ceec96
zerokun1091 <62707674+zerokun1091@users.noreply.github.com>
2020-04-06 19:55:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -40,11 +40,10 @@ async def on_message(message):
         args = message.content.split(" ")
 
         chname = message.guild.get_channel(args[1])
+        await chname.create_text_channel(name=args[2]) 
 
         if len(args) == 1:
             await message.channel.send("コマンドの引数が足りません。")
             return
-
-        await chname.create_text_channel(name=args[2])
 
 client.run(token)
